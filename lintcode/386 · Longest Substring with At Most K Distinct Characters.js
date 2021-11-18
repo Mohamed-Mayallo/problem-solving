2f5b9d1d6cc2ec037ca8a115d7bf4555e0b6cf46
Mohamed Mayallo <mayallo@baianat.com>
2021-11-18 17:16:28 +0200

    386 · Longest Substring with At Most K Distinct Characters

---
+++ lintcode/386 · Longest Substring with At Most K Distinct Characters.js
@@ -0,0 +1,55 @@
+/**
+ * 386 · Longest Substring with At Most K Distinct Characters
+ * Medium
+ * https://leetcode.com/problems/find-center-of-star-graph/
+ */
+
+/**
+ * First solution
+ * -----------------
+ * Time complexity  : O(N)
+ * Space complexity : O(1)
+ */
+
+export class Solution {
+    /**
+     * lengthOfLongestSubstringKDistinct
+     *
+     * @param s: A string
+     * @param k: An integer
+     * @return: An integer
+     */
+    lengthOfLongestSubstringKDistinct(s, k) {
+        let wStart = 0,
+            wMap = {},
+            wMapLen = 0,
+            mx = 0;
+
+        for (let wEnd = 0; wEnd < s.length; wEnd++) {
+            let wEndLtr = s[wEnd];
+
+            if (!wMap[wEndLtr]) {
+                wMap[wEndLtr] = 1;
+                wMapLen++;
+            } else {
+                wMap[wEndLtr]++;
+            }
+
+            while (wMapLen > k) {
+                let wStartLtr = s[wStart];
+                wMap[wStartLtr]--;
+
+                if (wMap[wStartLtr] < 1) {
+                    delete wMap[wStartLtr];
+                    wMapLen--;
+                }
+
+                wStart++;
+            }
+
+            mx = Math.max(mx, wEnd - wStart + 1);
+        }
+
+        return mx;
+    }
+}
