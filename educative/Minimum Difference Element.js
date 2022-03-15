aa885bcb3d8ad85d3103bfb0e98218f88ea988da
= <=>
2022-03-15 23:39:25 +0200

    Minimum Difference Element

---
+++ educative/Minimum Difference Element.js
@@ -0,0 +1,53 @@
+/**
+ * Minimum Difference Element
+ * Medium
+ * https://www.educative.io/courses/grokking-the-coding-interview/mymvP915LY9
+ */
+
+/**
+  Problem Statement
+
+  Given an array of numbers sorted in ascending order, find the element in the array that has the minimum difference with the given ‘key’.
+
+  Example 1:
+  Input: [4, 6, 10], key = 7
+  Output: 6
+  Explanation: The difference between the key '7' and '6' is minimum than any other number in the array 
+
+  Example 2:
+  Input: [4, 6, 10], key = 4
+  Output: 4
+
+  Example 3:
+  Input: [1, 3, 8, 10, 15], key = 12
+  Output: 10
+
+  Example 4:
+  Input: [4, 6, 10], key = 17
+  Output: 10
+ */
+
+/**
+ * First solution
+ * -----------------
+ * Time complexity  : O(Log(N))
+ * Space complexity : O(1)
+ */
+
+const search_min_diff_element = function (arr, key) {
+    let start = 0,
+        end = arr.length - 1;
+    if (key <= arr[start]) return arr[start];
+    if (key >= arr[end]) return arr[end];
+    while (end >= start) {
+        let mid = Math.floor((start + end) / 2);
+        let m = arr[mid];
+        if (m === key) return m;
+        else if (key > m) start = mid + 1;
+        else if (key < m) end = mid - 1;
+    }
+    if (Math.abs(key - arr[start]) >= Math.abs(key - arr[end])) {
+        return arr[end];
+    }
+    return arr[start];
+};
